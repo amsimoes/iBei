ad7a01ec6431e4f70d6582e8d99fd55abe41e684
A Simoes <asimoes@student.dei.uc.pt>
2016-12-18 17:48:24 +0000

    tcp server added

--- Meta2/sd/src/compileJava.sh
+++ Meta2/sd/src/compileJava.sh
@@ -2,6 +2,8 @@
 
 javac -cp "lib/*" iBei/aux/*.java
 javac iBei/rmi/*.java
+javac iBei/server/*.java
 
 jar cfm dataserver.jar manifests/manifest_rmi.mf iBei
+jar cfm server.jar manifests/manifest_tcpserver.mf iBei
 

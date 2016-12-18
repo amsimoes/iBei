d892966e2204e1fc5646b8f68eb2104ee280e9c6
A Simoes <asimoes@student.dei.uc.pt>
2016-12-18 18:57:16 +0000

    rmi interface checkFacebook

--- Meta2/sd/src/iBei/rmi/RMI_Interface.java
+++ Meta2/sd/src/iBei/rmi/RMI_Interface.java
@@ -35,6 +35,7 @@ public interface RMI_Interface extends Remote {
     public int statsLastWeek() throws RemoteException;
     public boolean banUser (String username) throws RemoteException;
     public boolean removeFacebook (String username) throws RemoteException;
+    public boolean checkFacebook(String username) throws RemoteException;
     public void checkBidNotf_clients() throws RemoteException;
     public void checkMsgNotf_clients() throws RemoteException;
     public ArrayList<ArrayList<Object>> checkBidNotf_clientsWebSockets() throws RemoteException;

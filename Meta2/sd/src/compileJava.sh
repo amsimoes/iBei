cebd3092513bef854a86f635bc3ac89b52dfcff7
A Simoes <asimoes@student.dei.uc.pt>
2016-12-18 02:29:51 +0000

    script updated for this meta

--- Meta2/sd/src/compileJava.sh
+++ Meta2/sd/src/compileJava.sh
@@ -1,10 +1,7 @@
 #! /bin/bash
 
 javac -cp "lib/*" iBei/aux/*.java
-javac  iBei/rmi/*.java
-javac iBei/server/*.java
-#javac iBei/admin/*.java
+javac iBei/rmi/*.java
 
-jar cfm dataserver.jar manifest_rmi.mf iBei
-jar cfm server.jar manifest_tcpserver.mf iBei
-#jar cfm admin.jar manifest_admin.mf iBei
+jar cfm dataserver.jar manifests/manifest_rmi.mf iBei
+
